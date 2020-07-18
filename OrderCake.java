7f29a514744e56f0543863783de805a10807a7ab
fazehpm99 <pirmohamadifaezeh@yahoo.com>
2020-07-18 16:09:31 +0430

    faezeh added ordercak

---
+++ OrderCake.java
@@ -0,0 +1,22 @@
+public class OrderCake extends Cake {
+    float weight;
+    public OrderCake(String n, double r) {
+        super(n, r);
+    }
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double calcPrice(/*float weight,int rate*/){
+        return weight*rate;
+    }
+
+    public OrderCake(float weight,int rate){
+        this.weight=weight;
+        this.rate=rate;
+    }
+}
